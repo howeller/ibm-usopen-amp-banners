b61fc080f20407d5a7d007877b8927e8820ecdf1
Chad Wienczkowski <chad.wienczkowski@hogarthww.com>
2021-08-11 15:23:08 -0400

    contentDate ud

--- preview/categories/Standard/contentData.js
+++ preview/categories/Standard/contentData.js
@@ -113,54 +113,6 @@ var objData = {
 				bannerList: [
 					{
 						overwriteSizeDisplay: "",
-						label: "IBM_USO_CloudAI_300x250",
-						src: "IBM_USO_CloudAI_300x250",
-						w: 300,
-						h: 250,
-						type: "html5",
-						approved: false,
-						dload: true,
-						backup: false,
-						manualzip: true
-					},
-					{
-						overwriteSizeDisplay: "",
-						label: "IBM_USO_CloudAI_300x300",
-						src: "IBM_USO_CloudAI_300x300",
-						w: 300,
-						h: 300,
-						type: "html5",
-						approved: false,
-						dload: true,
-						backup: false,
-						manualzip: true
-					},
-					{
-						overwriteSizeDisplay: "",
-						label: "IBM_USO_CloudAI_384x384",
-						src: "IBM_USO_CloudAI_384x384",
-						w: 384,
-						h: 384,
-						type: "html5",
-						approved: false,
-						dload: true,
-						backup: false,
-						manualzip: true
-					},
-					{
-						overwriteSizeDisplay: "",
-						label: "IBM_USO_CloudAI_600x500",
-						src: "IBM_USO_CloudAI_600x500",
-						w: 600,
-						h: 500,
-						type: "html5",
-						approved: false,
-						dload: true,
-						backup: false,
-						manualzip: true
-					},
-					{
-						overwriteSizeDisplay: "",
 						label: "IBM_USO_CloudAI_300x600",
 						src: "IBM_USO_CloudAI_300x600",
 						w: 300,
@@ -194,18 +146,6 @@ var objData = {
 						dload: true,
 						backup: false,
 						manualzip: true
-					},
-					{
-						overwriteSizeDisplay: "",
-						label: "IBM_USO_CloudAI_970x90",
-						src: "IBM_USO_CloudAI_970x90",
-						w: 970,
-						h: 90,
-						type: "html5",
-						approved: false,
-						dload: true,
-						backup: false,
-						manualzip: true
 					}
 				]
 			},
@@ -231,78 +171,6 @@ var objData = {
 						src: "IBM_USO_DigExperience_300x300",
 						w: 300,
 						h: 300,
-						type: "html5",
-						approved: false,
-						dload: true,
-						backup: false,
-						manualzip: true
-					},
-					{
-						overwriteSizeDisplay: "",
-						label: "IBM_USO_DigExperience_384x384",
-						src: "IBM_USO_DigExperience_384x384",
-						w: 384,
-						h: 384,
-						type: "html5",
-						approved: false,
-						dload: true,
-						backup: false,
-						manualzip: true
-					},
-					{
-						overwriteSizeDisplay: "",
-						label: "IBM_USO_DigExperience_600x500",
-						src: "IBM_USO_DigExperience_600x500",
-						w: 600,
-						h: 500,
-						type: "html5",
-						approved: false,
-						dload: true,
-						backup: false,
-						manualzip: true
-					},
-					{
-						overwriteSizeDisplay: "",
-						label: "IBM_USO_DigExperience_300x600",
-						src: "IBM_USO_DigExperience_300x600",
-						w: 300,
-						h: 600,
-						type: "html5",
-						approved: false,
-						dload: true,
-						backup: false,
-						manualzip: true
-					},
-					{
-						overwriteSizeDisplay: "",
-						label: "IBM_USO_DigExperience_970x250",
-						src: "IBM_USO_DigExperience_970x250",
-						w: 970,
-						h: 250,
-						type: "html5",
-						approved: false,
-						dload: true,
-						backup: false,
-						manualzip: true
-					},
-					{
-						overwriteSizeDisplay: "",
-						label: "IBM_USO_DigExperience_728x90",
-						src: "IBM_USO_DigExperience_728x90",
-						w: 728,
-						h: 90,
-						type: "html5",
-						approved: false,
-						dload: true,
-						backup: false,
-						manualzip: true
-					},
-					{
-						overwriteSizeDisplay: "",
-						label: "IBM_USO_DigExperience_970x90",
-						src: "IBM_USO_DigExperience_970x90",
-						w: 970,
-						h: 90,
 						type: "html5",
 						approved: false,
 						dload: true,
